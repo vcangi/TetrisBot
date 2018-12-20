6a14b31daba8738aa2762559e6637fbf45677a32
vcangi <vcangi96@gmail.com>
2018-12-20 14:39:24 -0500

    Removed Merge Warnings

--- Game/Block Types/J.py
+++ Game/Block Types/J.py
@@ -1,10 +1,5 @@
 class JBlock(block):
 	
-	def __init__(self):
-		self.__coord  = np.array([[5,0],[5,1],[4,2],[5,2]])
-		self.__color  = 5
-		self.__rotTF  = np.array([[2,0],[1,1],[0,0],[-1,1]])
-
 	def __init__(self,grid):
 		self.__coord = np.array([[5,0],[5,1],[4,2],[5,2]])
 		self.__color = 5
--- Game/Block Types/Z.py
+++ Game/Block Types/Z.py
@@ -1,17 +1,10 @@
 class ZBlock(block):
 	
-<<<<<<< HEAD
-	def __init__(self):
-		self.__coord = np.array([[4,0],[5,0],[5,1],[6,1]])
-=======
 	def __init__(self,grid):
 		self.__coord = np.array([[4,0],[0,5],[5,1],[6,1]])
->>>>>>> Added checks for valid block rotations/shifts
 		self.__color = 1
 		self.__orient = 0
 		self.__rotTF = np.array([[2,0],[1,1],[0,0],[-1,1]])
-<<<<<<< HEAD
 		self.__center = np.array([5, 1])
-=======
 		self.__grid = grid
->>>>>>> Added checks for valid block rotations/shifts
+
